980e62c27212e896498befcc9fb8e6be63f8e793
yebin <yebinp1926@gmail.com>
2024-06-15 16:14:15 +0900

    fix api server uni

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getCookie } from "../utils/cookie";
 
 // baseURL, credential, 헤더 세팅
-axios.defaults.baseURL = "http://localhost:8000/api";
+axios.defaults.baseURL = "/api";
 axios.defaults.withCredentials = true;
 axios.defaults.headers.post["Content-Type"] = "application/json";
 axios.defaults.headers.common["X-CSRFToken"] = getCookie("csrftoken");
